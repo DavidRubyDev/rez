2f2f940678cfcf0bba40d3fdbaca9f82a0318bbe
DavidRubyDev <davidrubydev@gmail.com>
2026-06-19 09:45:58 +0200

    Remove Infection, add PHP-CS-Fixer config, fix cs scripts

---
+++ .php-cs-fixer.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([__DIR__ . '/src', __DIR__ . '/tests']);
+
+return (new PhpCsFixer\Config())
+    ->setRules(['@PSR12' => true])
+    ->setFinder($finder);
