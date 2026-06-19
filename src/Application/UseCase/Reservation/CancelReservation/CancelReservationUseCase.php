db4e74a55f35a131c05bb5c2f720d0ca3de5ac91
DavidRubyDev <davidrubydev@gmail.com>
2026-06-19 20:45:59 +0200

    Add CancelReservationUseCaseTest

---
+++ src/Application/UseCase/Reservation/CancelReservation/CancelReservationUseCase.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rez\Application\UseCase\Reservation\CancelReservation;
+
+use Rez\Application\Port\ReservationRepositoryInterface;
+
+final class CancelReservationUseCase
+{
+    public function __construct(
+        private readonly ReservationRepositoryInterface $reservationRepository,
+    ) {
+    }
+
+    public function execute(CancelReservationRequest $request): CancelReservationResponse
+    {
+        $reservation = $this->reservationRepository->findById($request->reservationId);
+        $cancelled   = $reservation->cancel();
+
+        $this->reservationRepository->save($cancelled);
+
+        return new CancelReservationResponse($cancelled);
+    }
+}
