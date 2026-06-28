4c773b76cfd5bed3a284c70997346763c05381e9
David Ruby <davidrubydev@gmail.com>
2026-06-28 13:27:06 +0200

    Fix BUG-01: check resource exists before getting availability (404 on missing resource)

--- src/Application/UseCase/Availability/GetAvailability/GetAvailabilityUseCase.php
+++ src/Application/UseCase/Availability/GetAvailability/GetAvailabilityUseCase.php
@@ -5,18 +5,25 @@ declare(strict_types=1);
 namespace Rez\Application\UseCase\Availability\GetAvailability;
 
 use Rez\Application\Exception\DatabaseException;
+use Rez\Application\Port\ResourceRepositoryInterface;
 use Rez\Application\Service\AvailabilityServiceInterface;
 
 final class GetAvailabilityUseCase implements GetAvailabilityUseCaseInterface
 {
     public function __construct(
         private readonly AvailabilityServiceInterface $availabilityService,
+        private readonly ResourceRepositoryInterface $resourceRepository,
     ) {
     }
 
+    /**
+     * @throws \Rez\Domain\Exception\ResourceNotFoundException
+     * @throws DatabaseException
+     */
     public function execute(GetAvailabilityRequest $request): GetAvailabilityResponse
     {
         try {
+            $this->resourceRepository->findById($request->resourceId);
             $window = $this->availabilityService->getAvailableSlots(
                 $request->resourceId,
                 $request->date,
--- tests/Application/UseCase/Availability/GetAvailability/GetAvailabilityUseCaseTest.php
+++ tests/Application/UseCase/Availability/GetAvailability/GetAvailabilityUseCaseTest.php
@@ -8,23 +8,33 @@ use DateTimeImmutable;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Rez\Application\Exception\DatabaseException;
+use Rez\Application\Port\ResourceRepositoryInterface;
 use Rez\Application\Service\AvailabilityServiceInterface;
 use Rez\Application\UseCase\Availability\GetAvailability\GetAvailabilityRequest;
 use Rez\Application\UseCase\Availability\GetAvailability\GetAvailabilityUseCase;
 use Rez\Domain\Availability\AvailabilityWindow;
+use Rez\Domain\Exception\ResourceNotFoundException;
+use Rez\Domain\Resource\Resource;
 use Rez\Domain\Resource\ResourceId;
+use Rez\Domain\Resource\ResourceType;
 
 class GetAvailabilityUseCaseTest extends TestCase
 {
     private AvailabilityServiceInterface&MockObject $availabilityService;
+    private ResourceRepositoryInterface&MockObject $resourceRepository;
     private GetAvailabilityUseCase $useCase;
     private ResourceId $resourceId;
 
     protected function setUp(): void
     {
         $this->availabilityService = $this->createMock(AvailabilityServiceInterface::class);
-        $this->useCase             = new GetAvailabilityUseCase($this->availabilityService);
+        $this->resourceRepository  = $this->createMock(ResourceRepositoryInterface::class);
         $this->resourceId          = ResourceId::generate();
+
+        $resource = new Resource($this->resourceId, ResourceType::fromString('table'), 'Table 1', 4);
+        $this->resourceRepository->method('findById')->willReturn($resource);
+
+        $this->useCase = new GetAvailabilityUseCase($this->availabilityService, $this->resourceRepository);
     }
 
     public function testRepositoryDatabaseExceptionPropagates(): void
@@ -35,6 +45,17 @@ class GetAvailabilityUseCaseTest extends TestCase
 
         $this->expectException(DatabaseException::class);
         $this->expectExceptionMessage('Failed to get availability.');
+
+        $this->useCase->execute(new GetAvailabilityRequest($this->resourceId, new DateTimeImmutable('2024-01-15'), 60));
+    }
+
+    public function testThrowsResourceNotFoundExceptionForNonExistentResource(): void
+    {
+        $this->resourceRepository
+            ->method('findById')
+            ->willThrowException(new ResourceNotFoundException());
+
+        $this->expectException(ResourceNotFoundException::class);
 
         $this->useCase->execute(new GetAvailabilityRequest($this->resourceId, new DateTimeImmutable('2024-01-15'), 60));
     }
