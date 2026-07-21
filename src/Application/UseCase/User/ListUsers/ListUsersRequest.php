ad22590ef899dd3fb01b96a7fc8183f698281bd9
DavidRubyDev <davidrubydev@gmail.com>
2026-07-21 13:56:45 +0200

    Rewrite ListUsersUseCase for pagination/filtering/sorting (13_rez-pagination.md step 3)

--- src/Application/UseCase/User/ListUsers/ListUsersRequest.php
+++ src/Application/UseCase/User/ListUsers/ListUsersRequest.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace Rez\Application\UseCase\User\ListUsers;
 
+use Rez\Domain\User\UserRole;
+
 final class ListUsersRequest
 {
+    public function __construct(
+        public readonly ?string $search = null,
+        public readonly ?UserRole $role = null,
+        public readonly ?int $offset = null,
+        public readonly ?int $limit = null,
+        public readonly ?string $sortBy = null,
+        public readonly ?string $sortDir = null,
+    ) {
+    }
 }
